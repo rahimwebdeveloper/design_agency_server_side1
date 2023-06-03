40f5b8f128c30c3d3fa2d9bbc593b468de472c95
rahimwebdeveloper <inf.abdurrahim12@gmail.com>
2023-06-03 19:23:19 +0600

    heroku update

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ async function run() {
             res.send(services)
         });
 
+        console.log(services)
 
         //service get one item api 
         app.get('/service/:id', async (req, res) => {
